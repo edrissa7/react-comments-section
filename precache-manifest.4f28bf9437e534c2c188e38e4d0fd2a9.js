da4e4504f7d7d1a2fca0a48c758258ceab228432
Edrissa <ed.sarr7@gmail.com>
2023-04-22 09:23:29 +0000

    Updates

---
+++ precache-manifest.4f28bf9437e534c2c188e38e4d0fd2a9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b6a2b3eca6eee4fced6c5e831586d3c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5a3f50b33f31109bb88f",
+    "url": "./static/css/2.475b5157.chunk.css"
+  },
+  {
+    "revision": "32cc177fc2a9bcc483b4",
+    "url": "./static/css/main.f789d426.chunk.css"
+  },
+  {
+    "revision": "5a3f50b33f31109bb88f",
+    "url": "./static/js/2.3cbb1027.chunk.js"
+  },
+  {
+    "revision": "22facfebdf43ab8de268160bf7da3c75",
+    "url": "./static/js/2.3cbb1027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32cc177fc2a9bcc483b4",
+    "url": "./static/js/main.c37decae.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "./static/js/main.c37decae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f0c50b1f34893473cc7",
+    "url": "./static/js/runtime-main.3efce8c9.js"
+  },
+  {
+    "revision": "c25ed3f7f3be1809911a408c2002dce5",
+    "url": "./static/media/2a0bc34f2fdf6d6b.c25ed3f7.svg"
+  },
+  {
+    "revision": "d75833360cef7b8714624d330cc72328",
+    "url": "./static/media/a0e565557add9041.d7583336.svg"
+  },
+  {
+    "revision": "bc47e1a4f8d703c9fec4f39a499c7742",
+    "url": "./static/media/f86f932721d4cf30.bc47e1a4.svg"
+  }
+]);
